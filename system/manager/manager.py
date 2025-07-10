b3b9c1bee21feaa5b702c5563a204e3a84a8acdc
Nayan <nayan8teen@gmail.com>
2025-07-10 01:30:10 -0400

    UI: Custom Interactivity Timeout (#1032)
    custom interactivity timeout

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -55,6 +55,7 @@ def manager_init() -> None:
     ("DeviceBootMode", "0"),
     ("DynamicExperimentalControl", "0"),
     ("HyundaiLongitudinalTuning", "0"),
+    ("InteractivityTimeout", "0"),
     ("LagdToggle", "1"),
     ("Mads", "1"),
     ("MadsMainCruiseAllowed", "1"),
